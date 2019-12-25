0c4aacf4573e131b268718483e5cbaac4491f087
Jacob <jacobrosenthal@gmail.com>
2019-12-25 16:26:03 -0700

    game of life

---
+++ boards/pygamer/examples/game_of_life.rs
@@ -0,0 +1,87 @@
+//! Game of life from https://github.com/rustwasm/wasm_game_of_life
+
+#![no_std]
+#![no_main]
+
+#[allow(unused_imports)]
+use panic_halt;
+use pygamer as hal;
+
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::*;
+
+use hal::clock::GenericClockController;
+use hal::entry;
+use hal::pac::{CorePeripherals, Peripherals};
+
+use heapless::consts::*;
+use heapless::Vec;
+use itertools::Itertools;
+
+mod life;
+use life::{Cell, Universe};
+
+/// The width and height of the display
+const DISP_SIZE_X: i32 = 160;
+const DISP_SIZE_Y: i32 = 128;
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_internal_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.MCLK,
+        &mut peripherals.OSC32KCTRL,
+        &mut peripherals.OSCCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let mut pins = hal::Pins::new(peripherals.PORT).split();
+    let mut delay = hal::delay::Delay::new(core.SYST, &mut clocks);
+
+    let (mut display, _backlight) = pins
+        .display
+        .init(
+            &mut clocks,
+            peripherals.SERCOM4,
+            &mut peripherals.MCLK,
+            peripherals.TC2,
+            &mut delay,
+            &mut pins.port,
+        )
+        .unwrap();
+
+    //wasted space, but there is no U20480
+    let cells = Vec::<Cell, U32768>::new();
+    //more wasted space, a copy to edit while we keep last values
+    let next = Vec::<Cell, U32768>::new();
+
+    let mut universe = Universe::new(DISP_SIZE_X as u32, DISP_SIZE_Y as u32, cells, next);
+
+    loop {
+        universe.tick();
+        universe.draw(&mut display);
+    }
+}
+
+trait Viewer<T: embedded_graphics::pixelcolor::PixelColor> {
+    fn draw(&mut self, display: &mut impl DrawTarget<T>);
+}
+
+impl Viewer<Rgb565> for life::Universe {
+    fn draw(&mut self, display: &mut impl DrawTarget<Rgb565>) {
+        (0..self.height)
+            .cartesian_product(0..self.width)
+            .map(|(row, col)| {
+                let idx = self.get_index(row, col);
+                //feels odd, but x = column index, y=row_index
+                if self.cells[idx] == Cell::Alive {
+                    Pixel::<Rgb565>(Point::new(col as i32, row as i32), RgbColor::RED)
+                } else {
+                    Pixel::<Rgb565>(Point::new(col as i32, row as i32), RgbColor::BLACK)
+                }
+            })
+            .draw(display)
+            .ok();
+    }
+}
